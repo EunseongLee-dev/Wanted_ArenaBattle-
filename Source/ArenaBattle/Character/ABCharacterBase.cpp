7e5649cdbe7fc5d83829f2626f6c8f03ba7ec4e5
EunseongLee-dev <giver1902@gmail.com>
2026-04-20 10:53:21 +0900

    입력 컨트롤 전환을 위한 데이터 애셋 생성 및 구성
    - UABCharacterControlData 클래스 추가 및 속성 구성
    - UABCharacterControlData 기반 데이터 애셋 추가
    - IMC/Action 추가

---
+++ Source/ArenaBattle/Character/ABCharacterBase.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Character/ABCharacterBase.h"
+#include "ABCharacterControlData.h"
+
+// Sets default values
+AABCharacterBase::AABCharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// ¸Ê ¼³Á¤
+	static ConstructorHelpers::FObjectFinder<UABCharacterControlData> ShoulderDataRef(TEXT("/Game/ArenaBattle/CharacterControl/ABC_Shoulder.ABC_Shoulder"));
+	
+	if (ShoulderDataRef.Succeeded())
+	{
+		CharacterControlManager.Add(ECharacterControlType::Shoulder, ShoulderDataRef.Object);
+	}
+
+
+	static ConstructorHelpers::FObjectFinder<UABCharacterControlData> QuarterDataRef(TEXT("/Game/ArenaBattle/CharacterControl/ABC_Quater.ABC_Quater"));
+
+	if (QuarterDataRef.Succeeded())
+	{
+		CharacterControlManager.Add(ECharacterControlType::Quater, QuarterDataRef.Object);
+	}
+}
+
+void AABCharacterBase::SetCharacterControlData(const UABCharacterControlData* InCharacterControlData)
+{
+
+}
+
+
---
+++ Source/ArenaBattle/Character/ABCharacterBase.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ABCharacterBase.generated.h"
+
+// 열거형 (입력 컨트롤을 관리하기 위함)
+UENUM()
+enum class ECharacterControlType : uint8
+{
+	Shoulder,
+	Quater
+};
+
+UCLASS()
+class ARENABATTLE_API AABCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AABCharacterBase();
+
+protected:
+	// 컨트롤 데이터 설정
+	virtual void SetCharacterControlData(const class UABCharacterControlData* InCharacterControlData);
+
+protected:
+	// 컨트롤 타입 별 데이터 관리를 위한 맵
+	UPROPERTY(EditAnywhere, Category = CharacterControl)
+	TMap<ECharacterControlType, class UABCharacterControlData*> CharacterControlManager;
+
+};
